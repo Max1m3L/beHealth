21a4d2fade1eb0dc9b6a2f700095b9ddad85b6ad
Max1m <clashshow2000@gmail.com>
2023-08-09 16:29:48 +0300

    reviews

---
+++ functional/reviews.cjs
@@ -0,0 +1,31 @@
+// import axios from 'axios';
+
+
+const reviewUser1 = document.querySelector('.r-u1')
+const reviewUser2 = document.querySelector('.r-u2')
+const reviewUser3 = document.querySelector('.r-u3')
+const reviewUser4 = document.querySelector('.r-u4')
+
+// reviewUser1.innerHTML = axios.get('https://jsonplaceholder.typicode.com/users/1')
+//     .then(res => console.log(res))
+//     .catch(error => `пользователь не найден`)
+
+fetch('https://jsonplaceholder.typicode.com/users/2')
+    .then(res => res.json())
+    .then(value => reviewUser1.innerHTML = value.name) 
+    .catch(error => console.log(error))
+
+fetch('https://jsonplaceholder.typicode.com/users/3')
+    .then(res => res.json())
+    .then(value => reviewUser2.innerHTML = value.name) 
+    .catch(error => console.log(error))
+
+fetch('https://jsonplaceholder.typicode.com/users/4')
+    .then(res => res.json())
+    .then(value => reviewUser3.innerHTML = value.name) 
+    .catch(error => console.log(error))
+
+fetch('https://jsonplaceholder.typicode.com/users/5')
+    .then(res => res.json())
+    .then(value => reviewUser4.innerHTML = value.name) 
+    .catch(error => console.log(error))
